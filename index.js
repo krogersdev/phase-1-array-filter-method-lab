7aec85a1bac4b75924493121567819f3873fec61
Kevin Rogers <kevinarogers1@gmail.com>
2021-12-17 17:35:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(arr, driver) {
+    return arr.filter(el => el.toLowerCase().indexOf(driver.toLowerCase()) !== -1)
+}    
+
+function fuzzyMatch(arr, driver) {
+    return arr.filter(el => el.toLowerCase().indexOf(driver.toLowerCase()) === 0)
+}
+
+function matchName (arr, driver) {
+    let result = arr.filter(el => el.name === driver)
+    
+    if(result) {
+        return result;
+    }
+   
+}
